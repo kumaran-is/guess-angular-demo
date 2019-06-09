12cb11c7945871f2fa2075d2f9b920e182885924
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-06-09 08:50:55 -0400

    initial folder

---
+++ src/app/example/example.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ExampleRoutingModule } from './example-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ExampleRoutingModule
+  ]
+})
+export class ExampleModule { }
